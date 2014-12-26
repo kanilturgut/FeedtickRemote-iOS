8b1516c83dfbadbff12a3ad38ff996b9b90d1206
Kadir Anıl Turğut <kanilturgut@gmail.com>
2014-12-26 20:26:00 +0200

    devel

--- FeedtickRemote/PostController.m
+++ FeedtickRemote/PostController.m
@@ -22,12 +22,22 @@
 NSMutableArray *posts;
 NSString *journalId;
 
+UIColor *normalColor, *stickyColor, *approveColor, *denyColor, *blackColor, *whiteColor;
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     
+    normalColor     = [UIColor colorWithRed:0.996 green:0.996 blue:0.996 alpha:1];
+    stickyColor     = [UIColor colorWithRed:0.957 green:0.91 blue:0.353 alpha:1];
+    approveColor    = [UIColor colorWithRed:0.651 green:0.835 blue:0.373 alpha:1];
+    denyColor       = [UIColor colorWithRed:0.914 green:0.4 blue:0.322 alpha:1];
+    
+    blackColor      = [UIColor blackColor];
+    whiteColor      = [UIColor whiteColor];
+    
     _lblHeader.backgroundColor = [UIColor colorWithRed:0.255 green:0.792 blue:0.753 alpha:1];
+    
     
     journalId = hubId;
     posts = [NSMutableArray new];
@@ -63,32 +73,84 @@ NSString *journalId;
     NSString *feedText = [feed objectForKey:@"text"];
     NSString *feedUsername = [feed objectForKey:@"username"];
     NSNumber *status = [feed objectForKey:@"status"];
+    NSNumber *socialMediaType = [feed objectForKey:@"socialMediaType"];
     
     NSArray *imageURLs = [feed objectForKey:@"images"];
     NSString *imageURL = [imageURLs objectAtIndex:0];
     
+    int statusValue = status.intValue;
+    int socialMediaTypeValue = socialMediaType.intValue;
+    
     cell.tvPostText.text = feedText;
     cell.tvUsername.text = feedUsername;
     
-    int statusValue = status.intValue;
     UIImage *img = nil;
-    if (statusValue == MEDIA_TYPE_TWITTER)
+    if (socialMediaTypeValue == MEDIA_TYPE_TWITTER)
         img = [UIImage imageNamed:@"twitter_icon"];
-    else if (statusValue == MEDIA_TYPE_INSTAGRAM)
+    else if (socialMediaTypeValue == MEDIA_TYPE_INSTAGRAM)
         img = [UIImage imageNamed:@"instagram_icon"];
     
     cell.ivSocialMedia.image = img;
     [cell.ivPostImage setImageURL:[NSURL URLWithString:imageURL]];
     
+    cell.bSticky.layer.borderWidth = 1.0f;
+    cell.bSticky.layer.borderColor = [[UIColor grayColor] CGColor];
+    [cell.bSticky setTitleEdgeInsets:UIEdgeInsetsMake(5.0, 5.0, 5.0, 5.0)];
     cell.bSticky.tag = indexPath.row;
     [cell.bSticky addTarget:self action:@selector(stickyButtonClicked:) forControlEvents:UIControlEventTouchUpInside];
-    
+    [cell.bSticky setTitleColor:blackColor forState:UIControlStateNormal];
+    [cell.bSticky setTitleColor:whiteColor forState:UIControlStateSelected];
+
+    cell.bAprrove.layer.borderWidth = 1.0f;
+    cell.bAprrove.layer.borderColor = [[UIColor grayColor] CGColor];
+    [cell.bAprrove setTitleEdgeInsets:UIEdgeInsetsMake(5.0, 5.0, 5.0, 5.0)];
     cell.bAprrove.tag = indexPath.row;
     [cell.bAprrove addTarget:self action:@selector(approveButtonClicked:) forControlEvents:UIControlEventTouchUpInside];
+    [cell.bAprrove setTitleColor:blackColor forState:UIControlStateNormal];
+    [cell.bAprrove setTitleColor:whiteColor forState:UIControlStateSelected];
     
+    cell.bDeny.layer.borderWidth = 1.0f;
+    cell.bDeny.layer.borderColor = [[UIColor grayColor] CGColor];
+    [cell.bDeny setTitleEdgeInsets:UIEdgeInsetsMake(5.0, 5.0, 5.0, 5.0)];
     cell.bDeny.tag = indexPath.row;
     [cell.bDeny addTarget:self action:@selector(denyButtonClicked:) forControlEvents:UIControlEventTouchUpInside];
+    [cell.bDeny setTitleColor:blackColor forState:UIControlStateNormal];
+    [cell.bDeny setTitleColor:whiteColor forState:UIControlStateSelected];
 
+    
+    if (statusValue == STATUS_STICKY) {
+        cell.bSticky.backgroundColor = stickyColor;
+        cell.bAprrove.backgroundColor = normalColor;
+        cell.bDeny.backgroundColor = normalColor;
+        
+        [cell.bSticky setTitleColor:whiteColor forState:UIControlStateNormal];
+        [cell.bAprrove setTitleColor:blackColor forState:UIControlStateNormal];
+        [cell.bDeny setTitleColor:blackColor forState:UIControlStateNormal];
+    } else if (statusValue == STATUS_APPROVED) {
+        cell.bSticky.backgroundColor = normalColor;
+        cell.bAprrove.backgroundColor = approveColor;
+        cell.bDeny.backgroundColor = normalColor;
+        
+        [cell.bSticky setTitleColor:blackColor forState:UIControlStateNormal];
+        [cell.bAprrove setTitleColor:whiteColor forState:UIControlStateNormal];
+        [cell.bDeny setTitleColor:blackColor forState:UIControlStateNormal];
+    } else if (statusValue == STATUS_DENIED) {
+        cell.bSticky.backgroundColor = normalColor;
+        cell.bAprrove.backgroundColor = normalColor;
+        cell.bDeny.backgroundColor = denyColor;
+        
+        [cell.bSticky setTitleColor:blackColor forState:UIControlStateNormal];
+        [cell.bAprrove setTitleColor:blackColor forState:UIControlStateNormal];
+        [cell.bDeny setTitleColor:whiteColor forState:UIControlStateNormal];
+    } else {
+        cell.bSticky.backgroundColor = normalColor;
+        cell.bAprrove.backgroundColor = normalColor;
+        cell.bDeny.backgroundColor = normalColor;
+        
+        [cell.bSticky setTitleColor:blackColor forState:UIControlStateNormal];
+        [cell.bAprrove setTitleColor:blackColor forState:UIControlStateNormal];
+        [cell.bDeny setTitleColor:blackColor forState:UIControlStateNormal];
+    }
     
     return cell;
     
